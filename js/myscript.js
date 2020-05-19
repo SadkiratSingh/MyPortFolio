5ee12105be9d0f628c63f0e5df4c07f1b5c2e6c7
SadkiratSingh <sadkiratsingh@yahoo.com>
2020-05-19 18:11:49 +0530

    portfolio complete

--- js/myscript.js
+++ js/myscript.js
@@ -30,3 +30,51 @@ scrollToTop.addEventListener('click',function(){
 })
 
 
+const allSections=document.querySelectorAll('.section-wrapper');
+
+const projectLinks=document.querySelectorAll('.full-desc-link');
+
+const projects=document.querySelectorAll('.description-wrapper')
+
+const projectSection=document.querySelector('.project-desc')
+
+for(var i=0;i<projectLinks.length;i++){
+    projectLinks[i].addEventListener('click',noSectionDisplay);
+}
+
+function noSectionDisplay(e){
+
+    for(var i=0;i<allSections.length;i++){
+        allSections[i].style.display='none';
+    }
+    var classes=e.target.classList;
+    var p=classes[2];
+    projectSection.style.display='block'
+
+    for(var i=0;i<projects.length;i++){
+        if(projects[i].id!=p){
+            projects[i].style.display='none';
+        }
+        else{
+            projects[i].style.display='block';
+        }
+    }
+    document.documentElement.scrollTop=0;
+}
+
+const exitCross=document.querySelector('.img-desc-exit');
+const exitButton=document.querySelector('.desc-exit');
+exitCross.addEventListener('click',SectionDisplay);
+exitButton.addEventListener('click',SectionDisplay);
+
+function SectionDisplay(e){
+    
+    projectSection.style.display='none'
+    document.documentElement.scrollTop=0;
+    for(var i=0;i<allSections.length;i++){
+        allSections[i].style.display='block';
+    }
+    for(var i=0;i<projects.length;i++){
+        projects[i].style.display='none';
+    }
+}
